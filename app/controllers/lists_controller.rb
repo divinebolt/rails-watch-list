0866a8d407eb1a8f4a4f246eab8de69c5f86e95f
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-07-14 01:09:47 +0100

    my methods delete dont work

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,11 @@
 class ListsController < ApplicationController
+before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
     @bookmark = Bookmark.new
   end
 
@@ -23,12 +23,15 @@ class ListsController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:id])
     @list.destroy
     redirect_to lists_path
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
